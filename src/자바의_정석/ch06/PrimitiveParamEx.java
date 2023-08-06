c62785ab1cb10979a7142f7dce01976a3d998967
beautiflow <1114ny@naver.com>
2023-08-06 20:57:30 +0900

    자바의 정석 ch06.ex.6-9

---
+++ src/자바의_정석/ch06/PrimitiveParamEx.java
@@ -0,0 +1,24 @@
+package 자바의_정석.ch06;
+
+class Data{
+    int x;
+}
+
+public class PrimitiveParamEx {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+
+        }
+
+        static void change(int x){
+        x = 1000;
+            System.out.println("change() : x = " + x);
+        }
+    }
+
