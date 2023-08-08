01606372eeb0a036f804d0bab4856481cda6fa69
beautiflow <1114ny@naver.com>
2023-08-08 09:18:34 +0900

    자바의 정석 ch07.ex.7-18

---
+++ src/자바의_정석/ch07/BindingTest.java
@@ -0,0 +1,32 @@
+package 자바의_정석.ch07;
+
+public class BindingTest {
+    public static void main(String[] args) {
+        ParentB p = new ChildB();
+        ChildB c = new ChildB();
+
+        System.out.println("p.x = " + p.x);
+        p.method();
+
+        System.out.println("c.x = " + c.x);
+        c.method();
+    }
+
+}
+
+class ParentB{
+    int x = 100;
+
+    void method(){
+        System.out.println("Parent Method");
+    }
+}
+
+class ChildB extends ParentB{
+    int x = 200;
+
+    void method(){
+        System.out.println("Child Method");
+    }
+}
+
