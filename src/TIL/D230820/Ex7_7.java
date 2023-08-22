b5b7a47acd3d1a36347b536c03f514674385c3e3
beautiflow <1114ny@naver.com>
2023-08-22 11:29:16 +0900

    연습문제 7_7

--- src/TIL/D230820/Ex7_7.java
+++ src/TIL/D230820/Ex7_7.java
@@ -1,5 +1,7 @@
 package TIL.D230820;
 
+// commit + => 호출과정 추가 / 생성자가 호출되는 순서와 메서드가 실행되는 순서는 반대이다
+
 public class Ex7_7 {
     public static void main(String[] args) {
         Child c = new Child();
@@ -12,13 +14,16 @@ class Parent{
 
     Parent(){
         this(200);
+        System.out.println(4);
     }
 
     Parent(int x){
+        System.out.println(1);
         this.x = x;
     }
 
     int getX(){
+        System.out.println(2);
         return x;
     }
 }
@@ -28,9 +33,11 @@ class Child extends Parent{
 
     Child(){
         this(1000);
+        System.out.println(5);
     }
 
     Child(int x){
+        System.out.println(3);
         this.x = x;
     }
 }
