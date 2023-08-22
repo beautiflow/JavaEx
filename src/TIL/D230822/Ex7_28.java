e838bd10b4580623a7c2348198edbc6afe1a2b88
beautiflow <1114ny@naver.com>
2023-08-22 17:41:34 +0900

    연습문제 7_28

---
+++ src/TIL/D230822/Ex7_28.java
@@ -0,0 +1,26 @@
+package TIL.D230822;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Ex7_28 {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e){
+                e.getWindow().setVisible(false);
+                e.getWindow().dispose();
+                System.exit(0);
+            }
+        });
+    }
+}
+
+//class EventHandler extends WindowAdapter{
+//    public void windowClosing(WindowEvent e){
+//        e.getWindow().setVisible(false);
+//        e.getWindow().dispose();
+//        System.exit(0);
+//    }
+//}
